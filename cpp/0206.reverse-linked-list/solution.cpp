60183722befdc8825069ba812848e9bbf9f96bfd
EdwardWong1994 <e0641618@u.nus.edu>
2023-09-02 20:27:44 +0800

    Leetcode 206:翻转链表之递归实现（从前向后递归//从后向前递归）

--- cpp/0206.reverse-linked-list/solution.cpp
+++ cpp/0206.reverse-linked-list/solution.cpp
@@ -8,26 +8,66 @@ using namespace std;
 
 // @lc code=begin
 
-class Solution {
+//class Solution {
+//public:
+//    ListNode* reverseList(ListNode* head) {
+//        //使用循环不断反转列表，在翻转链表的时候需要一个tmp节点记住位置
+//        if(head ==nullptr)  return nullptr;
+//        if(head->next==nullptr) return head;   //如果只有一个元素，那么直接返回head就行
+//        ListNode* dummyNode = nullptr;
+//        ListNode* prev =dummyNode;
+//        ListNode* cur=head;
+//        while(cur->next!=nullptr){
+//            ListNode* tmp=cur->next;
+//            cur->next=prev;
+//            prev=cur;
+//ListNode* prev = nullptr;
+//ListNode* cur = head;
+//            cur=tmp;     
+//        }
+//        cur->next=prev;    // 最后要记住将最后一个节点连接上
+//        return cur;
+//    }
+//};
+
+
+//从前向后递归
+//
+//class Solution{
+//public:
+//
+//    ListNode*  reverse(ListNode* prev, ListNode* cur){
+//        if(cur==nullptr) return prev;    //记住这个地方要返回的是prev，而不是cur
+//        ListNode* tmp =cur->next;
+//        cur->next=prev;
+//        return reverse(cur,tmp);    //虽然这个reverse函数是有返回值的，但不是每次在调用自己时都需要给一个变量把他的值接住
+//    }
+//
+//    ListNode* reverseList(ListNode* head){
+//        //使用递归来解决问题
+//        //需要递归终止条件
+//        //if(head==nullptr)  return  nullptr;
+//        //if(head->next==nullptr) return head;
+//        return reverse(nullptr, head);
+//    }
+//};
+//
+// 从后向前翻转的递归实现
+class Solution{
 public:
-    ListNode* reverseList(ListNode* head) {
-        //使用循环不断反转列表，在翻转链表的时候需要一个tmp节点记住位置
-        if(head ==nullptr)  return nullptr;
-        if(head->next==nullptr) return head;   //如果只有一个元素，那么直接返回head就行
-        ListNode* dummyNode = nullptr;
-        ListNode* prev =dummyNode;
-        ListNode* cur=head;
-        while(cur->next!=nullptr){
-            ListNode* tmp=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=tmp;     
-        }
-        cur->next=prev;    // 最后要记住将最后一个节点连接上
-        return cur;
+    ListNode* reverseList(ListNode*  head){
+        //递归终止条件
+        if(head==nullptr)  return  head;
+        if(head->next==nullptr)  return head;
+        //翻转从第二个节点之后的链表
+        ListNode* node = reverseList(head->next);
+        //翻转第二个节点和第一个节点的指向
+        head->next->next=head;
+        //翻转第一个节点和第二个节点的指向
+        head->next=nullptr;
+        return node;
     }
 };
-
 // @lc code=end
 
 int main() {
@@ -35,6 +75,7 @@ int main() {
     stringstream out_stream;
 
     ListNode* head;
+    ListNode* prev =reverseList(head);
     LeetCodeIO::scan(cin, head);
 
     Solution *obj = new Solution();
