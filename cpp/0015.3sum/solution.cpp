fac76802dc535cae50f1b13131ebcbec7b8f1bd7
EdwardWong1994 <e0641618@u.nus.edu>
2023-09-10 17:37:22 +0800

    Leetcode 15: 使用双指针方法解决三数之和（关键在于去重）

--- cpp/0015.3sum/solution.cpp
+++ cpp/0015.3sum/solution.cpp
@@ -1,3 +1,4 @@
+
 // Created by EdwardWong1994 at 2023/09/10 14:31
 // leetgo: 1.3.8
 // https://leetcode.cn/problems/3sum/
@@ -11,26 +12,33 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        std::unordered_map<int,vector<int>>  mmap;   //key存放加数和，value存放索引
-        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>  result; 
+        //使用双指针的思路来解决问题
         for(int i=0;i<nums.size();++i){
-            //需要对i去重
-            if(i>=1 && nums[i-1]==nums[i])  continue;
-            for(int j=i+1;j<nums.size();++j){
-                if(j>=i+2 && nums[j]==nums[j-1]) continue;
-                mmap.insert(pair<int,vector<int>>(nums[i]+nums[j],{i,j}));
-                for(int z=j+1;z<nums.size();++z){
-                    auto it=mmap.find(-nums[z]);
-                    while(it!=mmap.end()){
-                        result.push_back({nums[(it->second)[0]],nums[(it->second)[1]],nums[z]});
-                        mmap.erase(it);
-                        it=mmap.find(-nums[z]);
-                    }
+            if(nums[i]>0) return result;
+            //需要对i进行去重操作，去重之前先要进行排序
+            if(i>0 && nums[i-1]==nums[i])  continue;
+            //定义一个left指针从i+1开始，另一个指针从nums.size()-1开始
+            int left=i+1;
+            int  right=nums.size()-1;
+            while(left<right){
+                if(nums[i]+nums[left]+nums[right]>0){
+                    right--;                    
+                }else if(nums[i]+nums[left]+nums[right]<0){
+                    left++;
+                }else{
+                    result.push_back({nums[i],nums[left],nums[right]});
+                    //对left和right进行剪枝操作
+                    while(left<right && nums[right]==nums[right-1])  right--;
+                    while(left<right && nums[left]== nums[left+1]) left++;
+                    right--;
+                    left++;
                 }
             }
         }
         return result;
-    } 
+    }
 };
 
 // @lc code=end
